c16dd38dbd02d36ceb07443cb7d488c8c120a5fe
Edouard Chang <edouard.chang@laposte.net>
2019-11-30 17:42:01 +0100

    4: ajouté un 2eme fond blurry derrières la première pour les écrans plus larges, enlvé la scrollbar, pour rajouté des fléches goup or godown

--- src/projet1-4/projet1-4.jsx
+++ src/projet1-4/projet1-4.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import $ from "jquery";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAngleDoubleDown,
+  faAngleDoubleUp
+} from "@fortawesome/free-solid-svg-icons";
 
 class Projet14 extends Component {
   state = {
@@ -8,6 +13,9 @@ class Projet14 extends Component {
     dateOpen: ""
   };
   componentDidMount() {
+    document.documentElement.style["scrollbar-width"] = "none";
+    document.documentElement.classList.add("noscroll");
+
     const d = new Date();
     this.setState({
       dateOpen: "Page ouverte à " + d.getHours() + " heures"
@@ -19,28 +27,10 @@ class Projet14 extends Component {
     }
 
     document.addEventListener("wheel", e => {
-      let nextPosition =
-        this.state.currentSection < 9
-          ? allSections[this.state.currentSection + 1].getBoundingClientRect()
-              .y + window.scrollY
-          : allSections[this.state.currentSection].getBoundingClientRect().y +
-            window.scrollY;
-      let previousPosition =
-        this.state.currentSection > 0
-          ? allSections[this.state.currentSection - 1].getBoundingClientRect()
-              .y + window.scrollY
-          : allSections[this.state.currentSection].getBoundingClientRect().y +
-            window.scrollY;
       if (e.deltaY > 0) {
-        window.scrollTo(0, nextPosition);
-        if (this.state.currentSection < 10) {
-          this.setState({ currentSection: this.state.currentSection + 1 });
-        }
+        this.goDownOne();
       } else if (e.deltaY < 0) {
-        window.scrollTo(0, previousPosition);
-        if (this.state.currentSection > 0) {
-          this.setState({ currentSection: this.state.currentSection - 1 });
-        }
+        this.goUpOne();
       }
     });
 
@@ -51,86 +41,134 @@ class Projet14 extends Component {
       this.setState({ timer: newTimer });
     }, 100);
   }
+
+  componentWillUnmount() {
+    document.documentElement.style["scrollbar-width"] = "auto";
+    document.documentElement.classList.remove("noscroll");
+  }
+
+  goDownOne = () => {
+    let allSections = document.getElementsByTagName("section");
+    let nextPosition =
+      this.state.currentSection < 10
+        ? allSections[this.state.currentSection + 1].getBoundingClientRect().y +
+          window.scrollY
+        : allSections[this.state.currentSection].getBoundingClientRect().y +
+          window.scrollY;
+    window.scrollTo(0, nextPosition);
+    if (this.state.currentSection < 10) {
+      this.setState({ currentSection: this.state.currentSection + 1 });
+    }
+  };
+
+  goUpOne = () => {
+    let allSections = document.getElementsByTagName("section");
+    let previousPosition =
+      this.state.currentSection > 0
+        ? allSections[this.state.currentSection - 1].getBoundingClientRect().y +
+          window.scrollY
+        : allSections[this.state.currentSection].getBoundingClientRect().y +
+          window.scrollY;
+    window.scrollTo(0, previousPosition);
+    if (this.state.currentSection > 0) {
+      this.setState({ currentSection: this.state.currentSection - 1 });
+    }
+  };
+
   render() {
     return (
-      <div className="inspiration">
-        <section id="0">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">Photo de chat juste parce que</div>
-          </div>
-        </section>
-        <section id="1">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">Un enfant qui rit</div>
-          </div>
-        </section>
-        <section id="2">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">Victoire</div>
-          </div>
-        </section>
-        <section id="3">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">Un gars content</div>
-          </div>
-        </section>
-        <section id="4">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">Au sommet du monde</div>
-          </div>
-        </section>
-        <section id="5">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">trouver image horizontale</div>
-          </div>
-        </section>
-        <section id="6">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">
-              La lumière est au bout du couloir
+      <div className="scroll">
+        <div className="inspiration">
+          <section id="0">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">
+                Photo de chat juste parce que
+                {this.state.currentSection !== 0 ? (
+                  <div className="goUpOne" onClick={this.goUpOne}>
+                    <FontAwesomeIcon icon={faAngleDoubleUp} />
+                  </div>
+                ) : null}
+                {this.state.currentSection !== 10 ? (
+                  <div className="goDownOne" onClick={this.goDownOne}>
+                    <FontAwesomeIcon icon={faAngleDoubleDown} />
+                  </div>
+                ) : null}
+              </div>
             </div>
-          </div>
-        </section>
-        <section id="7">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">blble</div>
-          </div>
-        </section>
-        <section id="8">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">trouver image horizontale</div>
-          </div>
-        </section>
-        <section id="9">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">Fin ?</div>
-          </div>
-        </section>
-        <section id="10">
-          <div className="filter d-flex">
-            <div className="quote m-auto h1">
-              <h1>Temps passé sur chaque image</h1>
-              <h5>
-                utilisation d'un simple setIntervale de 100ms en fonction du
-                currentSection
-              </h5>
-              {this.state.timer.map((sectionTimer, key) => {
-                return (
-                  <li key={key}>
-                    {key} :{" "}
-                    {sectionTimer % 1 !== 0
-                      ? sectionTimer
-                      : sectionTimer + ".0"}
-                  </li>
-                );
-              })}
-              <h6 id="ip">.</h6>
-              {/* <h6>Browser : {window.navigator.appCodeName}</h6>
-              <h6>OS : {window.navigator.oscpu}</h6>
-              <h6>{this.state.dateOpen}</h6> */}
+          </section>
+          <section id="1">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">Un enfant qui rit</div>
             </div>
-          </div>
-        </section>
+          </section>
+          <section id="2">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">Victoire</div>
+            </div>
+          </section>
+          <section id="3">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">Un gars content</div>
+            </div>
+          </section>
+          <section id="4">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">Au sommet du monde</div>
+            </div>
+          </section>
+          <section id="5">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">trouver image horizontale</div>
+            </div>
+          </section>
+          <section id="6">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">
+                La lumière est au bout du couloir
+              </div>
+            </div>
+          </section>
+          <section id="7">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">blble</div>
+            </div>
+          </section>
+          <section id="8">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">trouver image horizontale</div>
+            </div>
+          </section>
+          <section id="9">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">Fin ?</div>
+            </div>
+          </section>
+          <section id="10">
+            <div className="filter d-flex">
+              <div className="quote m-auto h1">
+                <h1>Temps passé sur chaque image</h1>
+                <h5>
+                  utilisation d'un simple setIntervale de 100ms en fonction du
+                  currentSection
+                </h5>
+                {this.state.timer.map((sectionTimer, key) => {
+                  return (
+                    <li key={key}>
+                      {key} :{" "}
+                      {sectionTimer % 1 !== 0
+                        ? sectionTimer
+                        : sectionTimer + ".0"}
+                    </li>
+                  );
+                })}
+                <h6 id="ip">.</h6>
+                <h6>Browser : {window.navigator.appCodeName}</h6>
+                <h6>OS : {window.navigator.oscpu}</h6>
+                <h6>{this.state.dateOpen}</h6>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
