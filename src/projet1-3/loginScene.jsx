54df57b568a7bf9f5d1f6b97897f8e71e17d907e
Edouard Chang <edouard.chang@laposte.net>
2019-12-07 17:10:14 +0100

    changed fitness login's visual

--- src/projet1-3/loginScene.jsx
+++ src/projet1-3/loginScene.jsx
@@ -13,27 +13,68 @@ class LoginScene extends Component {
         .getElementsByClassName("loginScene")[0]
         .classList.remove("opacityZero");
     }, 0);
+    document.getElementById("loginForm").style["height"] = "0rem";
   }
   onchange = e => {
     const newCredentials = { ...this.state.credentials };
     newCredentials[e.target.id] = e.target.value;
     this.setState({ credentials: newCredentials });
   };
+  clicking = () => {
+    let form = document.getElementById("loginForm");
+    form.style["height"] === "0rem"
+      ? (form.style["height"] = "10rem")
+      : (form.style["height"] = "0rem");
+
+    // document.getElementById("loginForm").style["opacity"] = "0";
+  };
   render() {
     return (
       <div className="loginScene">
-        <form onSubmit={this.props.checking} className="loginForm">
-          <input id="login" type="text" placeholder="account's name" />
-          <input id="pwd" type="password" placeholder="password" />
-          <input type="submit" value="Login" />
+        <h1>Fitness Log</h1>
+        <div onClick={this.clicking} className="btn">
+          <h3>Log in</h3>
+          <div className="arrowDown"></div>
+        </div>
+
+        <form
+          id="loginForm"
+          onSubmit={this.props.checking}
+          className="loginForm form-group overflow-hidden"
+        >
+          <input
+            id="login"
+            className="form-control"
+            type="text"
+            placeholder="account's name"
+          />
+          <input
+            id="pwd"
+            className="form-control"
+            type="password"
+            placeholder="password"
+          />
+          <input
+            type="submit"
+            className="form-control btn btn-dark"
+            value="Login"
+          />
+          {this.props.error ? (
+            <div
+              className="alert form-control alert-secondary p-1 text-center"
+              role="alert"
+            >
+              {this.props.error}
+            </div>
+          ) : (
+            <div className="alert form-control p-1 invisible" role="alert">
+              good
+            </div>
+          )}
         </form>
-        {this.props.error && (
-          <div className="alert alert-danger m-1 p-1" role="alert">
-            {this.props.error}
-          </div>
-        )}
-        <div>test account, login : Edouard, password : HAHA</div>
-        <div>next step account creation and cookies</div>
+
+        <div>test account | login : Edouard, password : HAHA</div>
+        <div>next step account creation</div>
       </div>
     );
   }
